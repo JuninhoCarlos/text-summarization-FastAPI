32ace126698496ec375c89ea2861755d94e9918e
Junior Carlos <junior.carlos@g.globo>
2022-07-25 11:15:47 -0300

    feat: initial setup

---
+++ project/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, FastAPI
+
+from app.config import get_settings, Settings
+
+
+app = FastAPI()
+
+
+@app.get("/ping")
+def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong",
+        "environment": settings.environment,
+        "testing": settings.testing,
+    }
